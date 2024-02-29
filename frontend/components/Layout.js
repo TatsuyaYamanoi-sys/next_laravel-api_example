60ee4bb530889dd579ffbf0afed5a5dc079a7cfe
TatsuyaYamanoi-sys <l.s.lueur@icloud.com>
2024-02-29 23:19:03 +0900

    feat: pageコンポーネント等 雛形を作成

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -31,17 +31,7 @@ const Layout = ({children, title="HP by Next.", description="example page."}) =>
         { children }
       </main>
       <footer className="w-full h-12 flex justify-center items-center border-t">
-        <a
-          className="flex items-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className="h-4 ml-2">
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+          <small>&copy; Tatsuya Yamanoi 2024</small>
       </footer>
     </div>
   )
